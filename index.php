579760feabeed993f83cf67019f5b1a25c254226
C-luigi <luigi.chiappa@le-campus-numerique.fr>
2023-06-29 10:40:01 +0200

    v1.2

--- index.php
+++ index.php
@@ -2,3 +2,19 @@
 echo 'Bienvenue sur le blog' ;
 ?>
 
+
+<?php
+$page = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_ENCODED);
+if ($page == "") {
+    require "action/page.php";
+}
+else if ($page == "hobby") {
+    require "action/page.php";
+}
+else if ($page == "contact") {
+    require "action/page.php";
+}
+else
+    require "action/page.php";
+
+?>
